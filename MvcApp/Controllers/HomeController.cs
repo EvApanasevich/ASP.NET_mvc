be36280bff4fed991413ecc694c4df15742d0a1f
Alexander Mamedov <smamedov451@gmail.com>
2023-11-27 15:02:41 +0300

    init commit

---
+++ MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly ITimeService timeService;
+
+        public HomeController(ITimeService timeServ)
+        {
+            timeService = timeServ;
+        }
+
+        public IActionResult Index(int age, string name)
+        {
+            //return new HtmlResult("<h2>Hello METANIT.COM!</h2><div>with html results</div>");
+
+            //if (age < 18) return Unauthorized(new Error("Access is denied"));
+            //if (string.IsNullOrEmpty(name)) return BadRequest("Name undefined");
+            //return Content($"Hello {(!string.IsNullOrEmpty(name) ? name : "no name")}! Your age is {age}");
+
+            return Ok("All is OK!");
+
+            //Person tom = new Person("Tom", 37);
+            //var jsonOptions = new System.Text.Json.JsonSerializerOptions
+            //{
+            //    PropertyNameCaseInsensitive = true, // не учитываем регистр
+            //    WriteIndented = true               // отступы для красоты
+            //};
+            //return Json(tom, jsonOptions);
+
+        }
+        public IActionResult About() 
+        {
+            return Content("About page");
+            //return Content($"Name:{name}  Age: {age}");
+            //return NotFound("Resource not found");
+        } 
+        public IActionResult Contact()
+        {
+            return Redirect("~/Home/About");
+            //return Redirect("https://microsoft.com");
+            //return RedirectToAction("About", "Home", new { name = "Tom", age = 37 });
+        }
+        public IActionResult GetFile()
+        {
+            // Путь к файлу
+            string file_path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files/document.docx");
+            // Тип файла - content-type
+            string file_type = "text/plain";
+            // Имя файла - необязательно
+            string file_name = "document.docx";
+
+            return PhysicalFile(file_path, file_type, file_name);
+        }
+        public IActionResult GetBytes()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files/pict.png");
+            byte[] mas = System.IO.File.ReadAllBytes(path);
+            string file_type = "application/octet-stream.";
+            string file_name = "pict.png";
+            return File(mas, file_type, file_name);
+        }
+        public IActionResult GetTime()
+        {
+            return Content(timeService.Time);
+        }
+    }
+    record class Person(string Name, int Age);
+    record class Error(string Message);
+}
---
+++ MvcApp/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();  // добавляем поддержку контроллеров
+builder.Services.AddTransient<ITimeService, SimpleTimeService>(); // добавляем сервис ITimeService
+
+var app = builder.Build();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+// В данном случае определен интерфейс ITimeService и его реализация SimpleTimeService. И в приложении происходит регистрация сервиса ITimeService.
+
+public interface ITimeService
+{
+    string Time { get; }
+}
+public class SimpleTimeService : ITimeService
+{
+    public string Time => DateTime.Now.ToString("hh:mm:ss");
+}
